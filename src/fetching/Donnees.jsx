96000e4be52a133257df9d0721a13a75b860f8d8
yatte shop <yatteshop@gmail.com>
2025-06-15 02:41:54 +0000

    test donnees

--- src/fetching/Donnees.jsx
+++ src/fetching/Donnees.jsx
@@ -1,5 +1,5 @@
 export const Donnees = async ()=>{
-  const res = await fetch("/.netlify/functions/produits")
+  const res = await fetch("https://yatteshop.pythonanywhere.com/api/shop/produit/")
   const response = await res.json()
   
   return response
